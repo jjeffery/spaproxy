060f6b3d78fe1bd5d670742ab4d6aa83e1bb978a
John Jeffery <jjeffery@sp.com.au>
2018-04-01 14:54:11 +1000

    Add goreleaser config.
    The idea is to use goreleaser to build a zip file that can be used directly as an AWS Lambda.

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/jjeffery/apigatewayproxy"
 	"github.com/jjeffery/kv"
@@ -13,7 +15,19 @@ import (
 	"github.com/jjeffery/spaproxy/handler"
 )
 
+// vars set by goreleaser
+var (
+	version = "unversioned"
+	commit  = "no-revision"
+	date    = "no-date"
+)
+
 func main() {
+	if len(os.Args) == 2 && os.Args[1] == "version" {
+		fmt.Println(versionText())
+		os.Exit(64) // sysexits.Usage
+	}
+
 	isLambda := apigatewayproxy.IsLambda()
 	if isLambda {
 		// cloudwatch logs already include the timestamp
@@ -24,7 +38,7 @@ func main() {
 		log.Fatalln("fatal:", err)
 	}
 
-	h, err := handler.New()
+	h, err := newHandler()
 	if err != nil {
 		log.Fatalln("fatal:", err)
 	}
@@ -56,4 +70,27 @@ func main() {
 		log.Fatalln("fatal: http server:", err)
 	}
 	log.Println("finished")
+}
+
+func newHandler() (http.Handler, error) {
+	h, err := handler.New()
+	if err != nil {
+		return nil, err
+	}
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/_spaproxy/version" && r.Method == http.MethodGet {
+			body := []byte(versionText())
+			w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+			w.Header().Set("Content-Length", strconv.Itoa(len(body)))
+			w.Write(body)
+			return
+		}
+
+		h.ServeHTTP(w, r)
+	}), nil
+}
+
+func versionText() string {
+	return fmt.Sprintf("version: %s\ncommit: %s\ndate: %s\n", version, commit, date)
 }
